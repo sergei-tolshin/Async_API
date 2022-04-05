6ce71e3015394b15c88b9a776a4fd22f39ce2fff
Yuri N. Matveev <ynmatveev@yandex.ru>
2022-04-05 22:29:38 +0300

    Добавлен backoff для проверки подключений в тестах

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,20 +1,23 @@
 import asyncio
+import logging
 import os
 
+import backoff
 from elasticsearch import AsyncElasticsearch
-
 
 ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
 ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
-es_client = AsyncElasticsearch(hosts=f'{ELASTIC_HOST}:{ELASTIC_PORT}')
 
 
+@backoff.on_exception(backoff.expo, ConnectionRefusedError, max_tries=20)
 async def main():
-    while not await es_client.ping():
-        await asyncio.sleep(1)
+    if not await es_client.ping():
+        raise ConnectionRefusedError("Connection to ES failed")
     await es_client.close()
-    print('close')
-
 
 if __name__ == '__main__':
+    es_client = AsyncElasticsearch(hosts=f'{ELASTIC_HOST}:{ELASTIC_PORT}')
+    logger = logging.getLogger('backoff')
+    logger.addHandler(logging.StreamHandler())
     asyncio.run(main())
+    logger.info('Сервис Elastic Search доступен')
--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,25 +1,26 @@
 import asyncio
+import logging
 import os
 
-import aioredis
-
+import backoff
+from aioredis import create_redis_pool
 
 REDIS_HOST: str = os.getenv('REDIS_HOST', '127.0.0.1')
 REDIS_PORT: int = int(os.getenv('REDIS_PORT', 6379))
 
 
+@backoff.on_exception(backoff.expo, ConnectionRefusedError, max_tries=20)
 async def main():
-    while True:
-        try:
-            redis = await aioredis.create_redis(f'redis://{REDIS_HOST}:{REDIS_PORT}')
-            redis.close()
-            await redis.wait_closed()
-            break
-        except:
-            await asyncio.sleep(1)
-
-    print('close')
-
+    redis = await create_redis_pool(
+            (REDIS_HOST, REDIS_PORT),
+            minsize=10,
+            maxsize=20
+        )
+    if redis:
+        redis.close()
 
 if __name__ == '__main__':
+    logger = logging.getLogger('backoff')
+    logger.addHandler(logging.StreamHandler())
     asyncio.run(main())
+    logger.info('Сервис Redis доступен')
